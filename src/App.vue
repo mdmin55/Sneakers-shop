10d0c70812a93b8fb487ea00c9f9df095928c87b
mdmin55 <oganyanmarina14@gmail.com>
2024-10-27 17:19:14 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { onMounted, ref, reactive} from 'vue';
+import axios from 'axios';
+import { watch } from 'vue';
+
+
+import Header from './components/Header.vue';
+import CardList from './components/CardList.vue';
+import Drawer from './components/Drawer.vue';
+
+const items = ref([]);
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) =>{
+  filters.sortBy = event.target.value
+}
+const onChangeSearchInput = (event) =>{
+  filters.searchQuery = event.target.value
+}
+const fetchItems = async() => {
+  try{
+    const params ={
+      sortBy: filters.sortBy
+    }
+    if (filters.searchQuery){
+      params.title = `*${filters.searchQuery}*`;
+    }
+    const {data} = await axios.get (`https://05d681b6ec2b8d1f.mokky.dev/Items`,
+    {
+    params
+    });
+    items.value=data
+    } catch (err){
+    console.log(err)
+    }}
+
+onMounted(fetchItems);
+watch(fetchItems);
+
+</script>
+
+<template>
+  <!-- <Drawer/> -->
+<div class="bg-white w-4/5 m-auto rounded-xl  shadow-xl mt-14">
+ <Header/> 
+  <div class="p-10">
+    <div class="flex justify-between ">
+      <h2 class="text-3xl font-bold mb-8 ">Все кроссовки</h2>
+      
+      <div class="flex gap-4">
+        <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none" >
+          <option value="name">По названию</option>
+          <option value="price">По цене(дешевые)</option>
+          <option value="-price">По цене (дорогие)</option>
+        </select>
+  
+        <div class="relative">
+          <img class="absolute left-3 top-3" src="/search.svg"  />
+          <input @input="onChangeSearchInput" class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" placeholder="Поиск...">
+        </div>
+      </div>
+   </div>
+   <CardList/>
+  </div>
+  <div class="mb-10">
+    <CardList  :items="items"/>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+     <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img class="w-10 " src="/logo.png" alt="Logo">
+      <dir >
+        <h2 class="text-xl font-bold uppercase">Your sneakers</h2>
+        <p class="text-slate-500 ">Магазин лучших кроссовок</p>
+      </dir>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black"> 
+        <img src="/cart.svg" alt="Cart">
+        <b>1205 руб.</b>
+      </li>
+      <li class="flex items-center  gap-3 cursor-pointer text-gray-500 hover:text-black" >
+        <img src="/heart.svg" alt="Cart">
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center  gap-3 cursor-pointer text-gray-500 hover:text-black" >
+        <img src="/profile.svg" alt="profil">
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+
+</template>
